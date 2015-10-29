a3d2393f887647974d80c1b52651e01d077f45aa
Stephen Gilboy <sgilboy@outlook.com>
2015-10-28 23:00:19 -0500

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    jade: {
+      debug: {
+        options: {
+          pretty: true
+        },
+        files: {
+          'build/debug/index.html' : 'src/index.jade'
+        }
+      },
+      release: {
+        options: {
+          pretty: true
+        },
+        files: {
+          'build/release/index.html' : 'src/index.jade'
+        }
+      }
+    },
+    copy: {
+      debug: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: 'theme/fonts/*',
+            dest: 'build/debug/fonts'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: 'theme/images/*',
+            dest: 'build/debug/images'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: [
+              'theme/scripts/jquery.min.js',
+              'theme/plugins/instafeed/instafeed.min.js',
+              'theme/plugins/magnific-popup/jquery.magnific-popup.min.js',
+              'theme/plugins/one-page-nav/jquery.nav.js',
+              'theme/plugins/owl-carousel/owl.carousel.min.js',
+              'theme/plugins/shuffle/jquery.shuffle.modernizr.min.js',
+              'theme/plugins/twitter-fetcher/twitterFetcher_min.js',
+              'theme/plugins/jquery-validate/jquery.validate.min.js',
+              'theme/plugins/wowjs/wow.min.js',
+              'theme/scripts/main.js'
+              ],
+            dest: 'build/debug/scripts'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: [
+              "theme/styles/animate.min.css",
+              "theme/styles/normalize.css",
+              "theme/plugins/magnific-popup/magnific-popup.css",
+              "theme/plugins/owl-carousel/owl.carousel.css",
+              "theme/styles/font-awesome.min.css",
+              "theme/styles/main.css"
+            ],
+            dest: 'build/debug/styles'
+          }
+        ]
+      }
+    },
+    htmlbuild: {
+      debug: {
+        src: 'build/debug/index.html',
+        dest: 'build/debug/index.html',
+        options: {
+          replace: true,
+          scripts: {
+            libs: [
+              'build/debug/scripts/jquery.min.js',
+              'build/debug/scripts/instafeed/instafeed.min.js',
+              'https://maps.googleapis.com/maps/api/js?v=3.exp',
+              'build/debug/scripts/jquery.magnific-popup.min.js',
+              'build/debug/scripts/jquery.nav.js',
+              'build/debug/scripts/owl.carousel.min.js',
+              'build/debug/scripts/jquery.shuffle.modernizr.min.js',
+              'build/debug/scripts/twitterFetcher_min.js',
+              'build/debug/scripts/jquery.validate.min.js',
+              'build/debug/scripts/wow.min.js',
+              'build/debug/scripts/main.js'
+            ]
+          },
+          styles: {
+            libraries: [
+              'build/debug/styles/*.css'
+            ]
+          }
+        }
+      },
+      release: {
+        
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-html-build');
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.registerTask('debug', ['jade:debug', 'copy:debug', 'htmlbuild:debug', 'serve']);
+};
---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Gilboy Portfolio Site
+=====================
+
+Using the [Unica](http://themeforest.net/item/unica-personal-onepage-html5-template/12951829?s_phrase=&s_rank=11) theme. Spend the $15 on it, I'm sure you can afford it if you're looking at the code.
